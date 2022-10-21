74b8960c376a10a4a260a951780b19df01ebf99b
Faridah <adebayofaridah99@gmail.com>
2022-10-20 19:25:06 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints integers
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	int n;
+
+	n = 0;
+	while
+		(n < 10)
+		{
+			_putchar(n + '0');
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
