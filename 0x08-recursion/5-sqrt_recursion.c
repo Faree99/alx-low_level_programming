5546c471de60c76b4725dc9a312305793aab2d00
Faridah <adebayofaridah99@gmail.com>
2022-11-02 21:52:40 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - evaluate squrt
+ * @n: integer
+ * Return: evaluate squrt
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n));
+	}
+
+}
