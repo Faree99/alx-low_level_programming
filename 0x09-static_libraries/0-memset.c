fa5efef1d037791185cd8cae3222a8981e62bea8
Faridah <adebayofaridah99@gmail.com>
2022-11-09 11:35:46 -0800

    Created a static library libmy.a

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -7,15 +7,14 @@
  * @n: bytes of memory to fill
  * Return: the memory area filled
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
 		s[a] = b;
 	}
-	
+
 	return (s);
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: char to be printed
- * Return: on success 1
+ * Return: 1 on success
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
