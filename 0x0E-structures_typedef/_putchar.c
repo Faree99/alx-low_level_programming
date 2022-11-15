5592788f753b452aa6bcb0b45ea4cbe3765deea1
Faridah <adebayofaridah99@gmail.com>
2022-11-15 12:00:45 -0800

    Task 0

--- 0x0E-structures_typedef/_putchar.c
+++ 0x0E-structures_typedef/_putchar.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on success 1 else -1
+ * Return: on success 1
+ * on error, -1 is returned, and error is set appropriately.
  */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
